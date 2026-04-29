bbfa9d4e7a8c1f4ba3006fff71a2720aa2bbcb79
Joeri G <43119241+Joeri-G@users.noreply.github.com>
2026-04-29 13:57:04 +0200

    defined market score function

--- src/ant/centralised.py
+++ src/ant/centralised.py
@@ -3,19 +3,23 @@ This module contains a number of centralised strategies
 """
 
 from .market import Market
+import cvxpy as cp
 
 """
 This function calculates the market equilibrium using the method described by Hellinga in 2025.
 """
-def P4(market: Market) -> float:
-    allocation_vector = [
-        ...
-    ]
-    score = 0
-    for agent in market.agents:
-        alpha = agent.resource_value() * agent.long_term_resource_endownment()
-        log_sum = 0
-        for other in agent.neighbours():
-            log_sum += other.resource_value() * allocation_vector[agent.id][other.id]
-        score += alpha * np.log(log_sum)
-    return score
+
+
+def P4(market: Market) -> List[List[int]]:
+    def market_score(allocation_matrix):
+        score = 0
+        for agent in market.agents:
+            alpha = agent.resource_value() * agent.long_term_resource_endownment()
+            log_sum = 0
+            for other in agent.neighbours():
+                log_sum += other.resource_value() * allocation_matrix[agent.id][other.id]
+            score += alpha * np.log(log_sum)
+        return score
+
+    allocation_matrix = cp.Variable((n, n))
+    
